cd3f3c084039e149585ab4f52b73804b5c8ba5ca
Riley Carlson <riley.carlson@ksb.com>
2023-08-30 13:05:30 -0400

    Implement time prediction for user images

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const width = 620
-const height = 480
-const startTime = new Date(2023,1,1);
-const endTime = new Date(2024,1,1);
+const width = 64
+const height = 64
+const startTime = new Date(2023,0,1);
+const endTime = new Date(2024,0,1);
 const colors = [
     // r,  g,  b,  a
     [  0,  0,  0,255], // black
+    [128,128,128,255], // white
     [255,255,255,255], // white
     [255,  0,  0,255], // red
     [  0,255,  0,255], // green
+    [255,255,  0,255], // yellow
     [  0,  0,255,255], // blue
+    [255,  0,255,255], // magenta
+    [  0,255,255,255], // cyan
   ];
 
 function percentElapsed() {
@@ -18,13 +22,36 @@ function percentElapsed() {
     return percentage;
   }
 function getClosestColor(rgba){
-    return colors.sort(color => {
-        const r = color[0] - rgba[0];
-        const g = color[1] - rgba[1];
-        const b = color[2] - rgba[2];
-        const a = color[3] - rgba[3];
-        return Math.sqrt(r^2 + g^2 + b^2 + a^2);
-    })[0];
+    const sortedColors = colors.sort((colorA, colorB) => {
+        const rA = colorA[0];
+        const gA = colorA[1];
+        const bA = colorA[2];
+        const aA = colorA[3];
+        
+        const rB = colorB[0];
+        const gB = colorB[1];
+        const bB = colorB[2];
+        const aB = colorB[3];
+        
+        const distanceA = Math.sqrt(
+            Math.pow(rA - rgba[0], 2) +
+            Math.pow(gA - rgba[1], 2) +
+            Math.pow(bA - rgba[2], 2) +
+            Math.pow(aA - rgba[3], 2)
+        );
+        
+        const distanceB = Math.sqrt(
+            Math.pow(rB - rgba[0], 2) +
+            Math.pow(gB - rgba[1], 2) +
+            Math.pow(bB - rgba[2], 2) +
+            Math.pow(aB - rgba[3], 2)
+        );
+        
+        return distanceA - distanceB;
+    });
+
+    const closestColor = sortedColors[0];
+    return closestColor;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -55,11 +82,39 @@ document.addEventListener("DOMContentLoaded", function () {
     // Handle image upload
     imageInput.addEventListener("change", function (event) {
         const file = event.target.files[0];
-        if (file) {
+        if (file && file.type.startsWith('image/')) {
             const reader = new FileReader();
             reader.onload = function (e) {
-                document.getElementById("userImage").src = e.target.result;
-                generateTextFromImage(file);
+                const img = new Image();
+                img.onload = function(){
+                    console.log("revising image...");
+                    const canvas = document.createElement('canvas');
+                    canvas.width = width;
+                    canvas.height = height;
+                    const context = canvas.getContext('2d');
+                    context.drawImage(img, 0, 0, width, height);
+                    var originalImgData = context.getImageData(0, 0, canvas.width, canvas.height).data;
+                    
+                    const imageData = context.createImageData(width, height);
+                    const data = imageData.data;
+                    console.log(`data length: ${data.length}`);
+                    for(let i = 0;i<data.length;i+=4){
+                        const newColor = getClosestColor([originalImgData[i+0],originalImgData[i+1],originalImgData[i+2],originalImgData[i+3]])
+                        data[i+0] = newColor[0];
+                        data[i+1] = newColor[1];
+                        data[i+2] = newColor[2];
+                        data[i+3] = newColor[3];
+                    }
+                    context.putImageData(imageData, 0, 0);
+
+                    console.log("loading into web page...");
+                    document.getElementById("userImage").src = "data:image/png;base64," + canvas.toDataURL("image/png").split(",")[1]; // You can change the format as needed
+                    document.getElementById("userImage").removeAttribute("hidden");
+                    document.getElementById("userImageLabel").textContent = `This image will appear at ${predictTime(data)}`;
+                    document.getElementById("userImageLabel").removeAttribute("hidden");
+                    console.log("done...");
+                }
+                img.src = e.target.result;
             };
             reader.readAsDataURL(file);
         }
@@ -118,6 +173,25 @@ function formatToScientificNotation(numberStr, significantDigits = 2) {
     return formattedNumber;
 }
 
+function predictTime(imageData){
+    let progress = 0;
+    const pixels = Math.floor(imageData.length/4); // imageData.width * imageData.height; //
+    for(let pixel=pixels-1;pixel>=0;pixel--){
+        const dataStart = pixel * 4;
+        const color = [
+            imageData[dataStart+0],
+            imageData[dataStart+1],
+            imageData[dataStart+2],
+            imageData[dataStart+3],
+        ]
+        const index = colors.findIndex(c => JSON.stringify(c) === JSON.stringify(color))
+        progress += index;
+        progress /= colors.length;
+    }
+    const length = endTime - startTime;
+    return new Date(startTime.getTime() + (length * progress));
+}
+
 function generateImage(progress) {
     // Example: Generating a simple image with a base64-encoded PNG
     const canvas = document.createElement("canvas");
@@ -149,13 +223,4 @@ function generateImage(progress) {
         context.putImageData(imageData, 0, 0);
     }
     return canvas.toDataURL("image/png").split(",")[1]; // Return the base64-encoded image data
-}
-
-function generateTextFromImage(imageFile) {
-    // Example: Generate text based on the uploaded image's name
-    document.getElementById("userImageLabel").textContent = `You uploaded a file: ${imageFile}`;
-}
-
-function setUserImage(imageFile){
-    return imageFile;
 }
